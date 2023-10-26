abc0ada09a972b941e10c9d1f99afe63f257733c
Keyance <klara.konopacova@seznam.cz>
2023-10-26 10:11:03 +0200

    Přidejte soubory projektu.

---
+++ L3ukol2/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using System.IO;
+using System;
+
+string cesta = @"e:\text\czechitas\text2.txt";
+
+if (File.Exists(cesta))
+{
+    Console.WriteLine("soubor existuje.");
+    File.WriteAllText(cesta, "");
+}
+else
+{
+    Console.WriteLine("soubor neexistuje.");
+}
+
+
+
+
+while (true)
+{
+    string text = Console.ReadLine();
+    File.AppendAllText(cesta, text+Environment.NewLine);
+
+    if (string.IsNullOrEmpty(text))
+    {
+        break;
+
+    }
+}
+
+Console.WriteLine("Tady je obsah textového souboru:");
+Console.WriteLine(File.ReadAllText(cesta));
+Console.ReadLine();
